3e38eda60bc3383f52504834001e924f3a8d8d91
raulbborba19 <raul.borba@escola.pr.gov.br>
2025-09-02 11:04:24 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,116 @@
-
+const perguntas = [
+    {
+        enunciado: "pergunta 1",  //a pergunta
+        alternativas: [
+            {
+                texto: "",  //primeira alternativa
+                afirmacao: [
+                    "",  //comentário para a resposta
+                    ""   //segundo comentário
+                ]
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    ""
+                    ""
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            {
+                texto: "",  
+                afirmacao: [
+                    "",  
+                    ""   
+                ]
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    ""
+                    ""
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 4",
+        alternativas: [
+            {
+                texto: "",  
+                afirmacao: [
+                    "",  
+                    ""   
+                ]
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    ""
+                    ""
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            {
+                texto: "",  
+                afirmacao: [
+                    "",  
+                    ""   
+                ]
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    ""
+                    ""
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",  
+                afirmacao: [
+                    "",  
+                    ""   
+                ]
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    ""
+                    ""
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 5",
+        alternativas: [
+            {
+                texto: "",  
+                afirmacao: [
+                    "",  
+                    ""   
+                ]
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    ""
+                    ""
+                ]
+            }
+        ]
+    },
+]
