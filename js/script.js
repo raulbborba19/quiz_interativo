3e81c60b072b94324a52a78acd3d733747a454cc
raulbborba19 <raul.borba@escola.pr.gov.br>
2025-09-03 07:39:47 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+fuction mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativa();
+}
 
+function mostraAlternativa() {}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = `Após tudo isso, ${nome} descobriu que`;
+    textResultado
+}
