43ff96ce02a5db3113abc410d67e412e54399e5f
raulbborba19 <raul.borba@escola.pr.gov.br>
2025-09-10 07:33:40 -0300

    .

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPrincipal = document.querySelector(".caixa-principal");
+const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
+const botaoIniciar = document.querySelector(".iniciar-");
+const telaInicial = document.querySelector(".-");
 
 let atual = 0;
 let perguntaAtual;
@@ -16,6 +18,7 @@ botaoIniciar.addEventListener('click', iniciaJogo);
 
 function iniciaJogo() {
     atual = 0;
+    historiaFinal = "";
     teleInicial.style.display = 'none';
     caixaPerguntas.classList.remove(".mostrar")
     caixaAlternativas.classList.remove(".mostrar")
@@ -34,7 +37,12 @@ function mostraPergunta() {
     mostraAlternativa();
 }
 
-function mostraAlternativa() {}
+function mostraAlternativa() {
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent
+    }
+}
 
 function mostraResultado() {
     caixaPerguntas.textContent = `Após tudo isso, ${nome} descobriu que`;
