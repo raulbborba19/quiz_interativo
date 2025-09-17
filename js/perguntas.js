53ba55149269359a559e83feb513a61856d8bace
raulbborba19 <raul.borba@escola.pr.gov.br>
2025-09-17 07:27:01 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,83 +1,83 @@
 const perguntas = [
     {
-        enunciado: "pergunta 1",  //a pergunta
+        enunciado: "Você prefere batata frita ou nuggets?",  //a pergunta
         alternativas: [
             {
-                texto: "",  //primeira alternativa
+                texto: "Batata frita",  //primeira alternativa
                 afirmacao: [
-                    "",  //comentário para a resposta
-                    ""   //segundo comentário
+                    "Você tem bom gosto!",  //comentário para a resposta
+                    "Otima escolha!"   //segundo comentário
                 ],
                 proxima 1,
             },
             {
-                texto: "",
+                texto: "Nuggets",
                 afirmacao: [
-                    ""
-                    ""
+                    "Não é o melhor."
+                    "Você realmente gosta disso?"
                 ],
                 proxima 1,
             }
         ]
     },
     {
-        enunciado: "pergunta 2",
+        enunciado: "Você prefere shopping ou parque?",
         alternativas: [
             {
-                texto: "",  
+                texto: "Shopping",  
                 afirmacao: [
-                    "",  
-                    ""   
+                    "Você é consumista kaka",  
+                    "Você precisa tomar sol."   
                 ],
                 proxima 2,
             },
             {
-                texto: "",
+                texto: "Parque",
                 afirmacao: [
-                    ""
-                    ""
+                    "Você realmente gosta disso?"
+                    "Otima escolha."
                 ],
                 proxima 2,
             }
         ]
     },
     {
-        enunciado: "pergunta 4",
+        enunciado: "Você prefere refrigerante ou água?",
         alternativas: [
             {
-                texto: "",  
+                texto: "Refrigerante",  
                 afirmacao: [
-                    "",  
-                    ""   
+                    "Sabe que não é saudavel.",  
+                    "Somo dois );"   
                 ],
                 proxima 3,
             },
             {
-                texto: "",
+                texto: "Água",
                 afirmacao: [
-                    ""
-                    ""
+                    "Você é saudavel!"
+                    "Otima escolha!"
                 ],
                 proxima 3,
             }
         ]
     },
     {
-        enunciado: "pergunta 2",
+        enunciado: "Você chama de Biscoito ou Bolacha.",
         alternativas: [
             {
-                texto: "",  
+                texto: "Biscoito",  
                 afirmacao: [
-                    "",  
-                    ""   
+                    "Tem certeza?",  
+                    "Se você acha..."   
                 ],
                 proxima 4,
             },
             {
-                texto: "",
+                texto: "Bolacha",
                 afirmacao: [
-                    ""
-                    ""
+                    "Se você acha..."
+                    "Tem certeza?"
                 ],
                 proxima 4,
             }
